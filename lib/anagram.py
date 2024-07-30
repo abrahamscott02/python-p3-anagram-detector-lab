1fa58d90b187987557dfe5599f930878972b6b1f
Abraham Scott <abrahamscott02@gmail.com>
2024-07-30 09:52:26 -0600

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        def sorted_letters(word):
+            return sorted(word.lower())
+
+        base_word_sorted = sorted_letters(self.word)
+        return [word for word in words if sorted_letters(word) == base_word_sorted]
